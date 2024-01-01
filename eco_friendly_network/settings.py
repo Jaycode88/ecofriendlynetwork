80a2f65d66d3365b4ab3182eb84d476610682b6c
Joe Seabrook <joeseabrook0306@gmail.com>
2024-01-01 14:20:19 +0000

    Change Debug settings

--- eco_friendly_network/settings.py
+++ eco_friendly_network/settings.py
@@ -24,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['8000-jaycode88-ecofriendlyne-9c3xmnw9i4g.ws-eu107.gitpod.io', 'ecofriendlynetwork-82b71b042579.herokuapp.com']
 
