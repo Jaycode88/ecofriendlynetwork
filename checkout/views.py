59206cd634d18f123b197593f0616b060e4d4b1f
Joe Seabrook <joeseabrook0306@gmail.com>
2024-01-05 13:56:16 +0000

    Edit checkout view to prepopulate form from prof

--- checkout/views.py
+++ checkout/views.py
@@ -97,7 +97,27 @@ def checkout(request):
         currency=settings.STRIPE_CURRENCY,
     )
 
-    order_form = OrderForm()
+    # Check if the user is authenticated and has a profile
+    if request.user.is_authenticated:
+        try:
+            profile = UserProfile.objects.get(user=request.user)
+            # Pre-populate the order form with user's profile information
+            form_data = {
+                'full_name': profile.user.get_full_name(),
+                'email': profile.user.email,
+                'phone_number': profile.default_phone_number,
+                'country': profile.default_country,
+                'postcode': profile.default_postcode,
+                'town_or_city': profile.default_town_or_city,
+                'street_address1': profile.default_street_address1,
+                'street_address2': profile.default_street_address2,
+                'county': profile.default_county,
+            }
+            order_form = OrderForm(initial=form_data)
+        except UserProfile.DoesNotExist:
+            order_form = OrderForm()
+    else:
+        order_form = OrderForm()
 
     if not stripe_public_key:
         messages.warning(request, 'Stripe public key is missing. \
