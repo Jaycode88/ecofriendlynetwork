aaa8da326eec67dd49891e5fb0c57a30c7b2273b
Joe Seabrook <joeseabrook0306@gmail.com>
2024-01-09 11:49:42 +0000

    Improve Admin area Orders

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,5 +1,24 @@
 from django.contrib import admin
+from django.http import HttpResponse
+import csv
 from .models import Order, OrderLineItem
+
+def export_orders_to_csv(modeladmin, request, queryset):
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename="orders.csv"'
+    writer = csv.writer(response)
+
+    # Write CSV headers
+    writer.writerow(['Order Number', 'Date', 'Full Name', 'Total'])
+
+    # Write data
+    for order in queryset:
+        writer.writerow([order.order_number, order.date, order.full_name, order.grand_total])
+
+    return response
+
+export_orders_to_csv.short_description = 'Export to CSV'
+
 
 class OrderLineItemAdminInLine(admin.TabularInline):
     model = OrderLineItem
@@ -25,5 +44,12 @@ class  OrderAdmin(admin.ModelAdmin):
                     'grand_total')
 
     ordering = ('-date',)
+
+    # Enhanced search fields
+    search_fields = ('order_number', 'full_name', 'email', 
+                     'phone_number', 'postcode', 'town_or_city', 
+                     'street_address1', 'street_address2')
+
+    actions = [export_orders_to_csv]
 
 admin.site.register(Order, OrderAdmin)
