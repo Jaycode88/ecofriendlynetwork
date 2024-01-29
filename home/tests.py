db1cd46554f64064d7b625532f8bf570e1fa194b
Joe Seabrook <joeseabrook0306@gmail.com>
2024-01-29 14:03:08 +0000

    Add Python tests to Home app

--- home/tests.py
+++ home/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+class HomeViewsTestCase(TestCase):
+
+    def test_index_view_exists(self):
+        """
+        Test to ensure the index view exists and is accessible.
+        """
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_index_view_uses_correct_template(self):
+        """
+        Test to ensure the index view uses the correct template.
+        """
+        response = self.client.get(reverse('home'))
+        self.assertTemplateUsed(response, 'home/index.html')
+
+    def test_index_view_context(self):
+        """
+        Test to ensure the index view passes the correct context to the template.
+        (Modify this if your view passes context)
+        """
+        response = self.client.get(reverse('home'))
